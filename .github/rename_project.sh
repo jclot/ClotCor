22bc8b399c628e2fc4090edb900064a9476cf8b0
jclot <jclot@users.noreply.github.com>
2024-09-02 07:17:26 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="jclot"
+original_name="clotcor"
+original_urlname="ClotCor"
+original_description="Awesome clotcor created by jclot"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv clotcor $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ clotcor/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for clotcor."""
+
+from clotcor.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
